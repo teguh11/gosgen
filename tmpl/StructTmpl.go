0ac80b99e69c4fe087498a2b5208d05290416da7
teguh <teguh@bareksa.com>
2020-04-20 22:40:46 +0700

    structtmpl desc

--- tmpl/StructTmpl.go
+++ tmpl/StructTmpl.go
@@ -1,15 +1,18 @@
 package tmpl
 
+// StructTmpl for define Struct Name
 type StructTmpl struct {
 	StructName string
 	FieldName  []StructField
 }
 
+// StructField for define Struct data
 type StructField struct {
 	Name     string
 	DataType string
 }
 
+// StructTemplate is function for generating struct based on StructTmpl and StructField
 func StructTemplate() []byte {
 	return []byte(`package models
 
